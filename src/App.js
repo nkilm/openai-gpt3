09a72f237c8d66897111e090e6015277b213d821
nkilm <nikhilmohitelhs@gmail.com>
2022-07-02 12:55:14 +0530

    add assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react'
+
+import {Blog,Features,Footer,Header,Possibility,WhatGPT3} from "./containers";
+import {CTA,Brand,Navbar} from "./components";
 
 function App() {
   return (
-    <div>
-      <h1>GPT3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
