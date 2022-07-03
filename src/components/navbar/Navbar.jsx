9e37f3b335f3eb73120fff685fe4df550302fb21
nkilm <nikhilmohitelhs@gmail.com>
2022-07-03 12:41:23 +0530

    responsive css for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.css"
 import logo from "../../assets/logo.svg"
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
+
+const Menu = () => (
+  <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#wgpt3'>What is GPT3?</a></p>
+    <p><a href='#possibility'>Open AI</a></p>
+    <p><a href='#features'>Case Studies</a></p>
+    <p><a href='#blog'>Library</a></p>
+  </>
+)
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -10,15 +22,30 @@ const Navbar = () => {
           <img src={logo} alt="GPT3 Logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p><a href='#home'>Home</a></p>
-          <p><a href='#wgpt3'>What is GPT3?</a></p>
-          <p><a href='#possibility'>Open AI</a></p>
-          <p><a href='#features'>Case Studies</a></p>
-          <p><a href='#blog'>Library</a></p>
+          <Menu />
         </div>
         <div className="gpt3__navbar-sign">
           <p>Sign in</p>
           <button type='button'>Sign up</button>
+        </div>
+        {/* Responsive part for mobiles devices */}
+        {/* hamburger menu */}
+        <div className="gpt3__navbar-menu">
+          {toggleMenu
+            ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+            : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+          }
+          {toggleMenu &&
+            <div className="gpt3__navbar-menu_container scale-up-center">
+              <div className="gpt3__navbar-menu_container-links">
+                <Menu />
+              </div>
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
+              </div>
+            </div>
+          }
         </div>
       </div>
     </div>
