0809daf59431f88c9c4f09681f2f75370c2c9637
nkilm <nikhilmohitelhs@gmail.com>
2022-07-05 00:36:08 +0530

    cta section with responsive css

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,13 @@ import "./cta.css"
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className="gpt3__cta section__margin">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h1>Register today & start exploring the endless possiblities.</h1>
+      </div>
+      <button>Get Started</button>
+    </div>
   )
 }
 
