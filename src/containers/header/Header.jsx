a3789fa83cea30237ee4d04ad0bbd509d752ebda
nkilm <nikhilmohitelhs@gmail.com>
2022-07-04 20:39:12 +0530

    update header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="gpt3__header section_padding" id='home'>
       <div className="gpt3__header-content">
         <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores voluptas earum sed tenetur ipsam quos doloribus eaque numquam, fuga fugiat, fugit reprehenderit voluptate vel sequi sunt, iusto in beatae tempore. Eius repudiandae reprehenderit sapiente qui? Voluptatem nemo necessitatibus ea velit cupiditate in eum quam! Temporibus necessitatibus optio consequatur quod dicta.</p>
+        <p>The first version of GPT was released in 2018 and contained 117 million parameters. The second version of the model, GPT-2, was released in 2019 with around 1.5 billion parameters. As the latest version, GPT-3 jumps over the last model by a huge margin with over 175 billion parameters, which is more than 100 times its predecessor and ten times more than comparable programs.</p>
 
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your Email Address" />
