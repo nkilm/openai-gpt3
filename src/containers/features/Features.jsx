c46deba6a3e577aa285434aa43da7d540c9a03c7
nkilm <nikhilmohitelhs@gmail.com>
2022-07-04 21:45:03 +0530

    add id to features

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -23,7 +23,7 @@ const featuresData = [
 
 const Features = () => {
   return (
-    <div className="gpt3__features section__padding">
+    <div className="gpt3__features section__padding" id="features">
       <div className="gpt3__features-heading">
         <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
         <p>Request Early Access to Get Started</p>
