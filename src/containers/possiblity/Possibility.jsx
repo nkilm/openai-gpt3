0be443303ed21a15e7907eaa168b072280cfa6ec
nkilm <nikhilmohitelhs@gmail.com>
2022-07-04 22:45:09 +0530

    possibility section

--- src/containers/possiblity/Possibility.jsx
+++ src/containers/possiblity/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./possibility.css"
 
+import  possibilityImage  from '../../assets/possibility.png'
+
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className="gpt3__possibility section__padding" id='possibility'>
+        <div className="gpt3__possibility-image">
+          <img src={possibilityImage} alt="possibility" />
+        </div>
+        <div className="gpt3__possibility-container">
+          <p><a href="/">Request Early Access to Get Started</a></p>
+          <h1>The possibilities are beyond your imagination.</h1>
+          <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        </div>
+    </div>
   )
 }
 
