d4d7da6945e55c68170569c584b1a8a34aed337b
nkilm <nikhilmohitelhs@gmail.com>
2022-07-05 10:50:50 +0530

    Blog section with responsive css

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Article } from '../../components'
 import "./blog.css"
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports.js"
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className="gpt3__blog section__padding">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={blog01} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={blog02} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={blog03} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={blog04} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={blog05} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+      </div>
+    </div>
   )
 }
 
