9672f4895c39b9e3fcb3f4b1b6666ef7933ed511
nkilm <nikhilmohitelhs@gmail.com>
2022-07-05 13:17:22 +0530

    footer section with responsive css

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import "./footer.css"
+import logoUrl from "../../assets/logo.svg"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h3 className="gradient__text">
+          Do you want to step in to the future before others?
+        </h3>
+      </div>
+      <div className="gpt3__footer-button">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={logoUrl} alt="gpt3 logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h3>Links</h3>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h3>Company</h3>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h3>Get in touch</h3>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyrights">
+        ©2022 GPT-3. All rights reserved.
+      </div>
+    </div>
   )
 }
 
