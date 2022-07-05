c38b8390ad4faff931a62c2e9fd9ffbfa73b5387
nkilm <nikhilmohitelhs@gmail.com>
2022-07-05 10:50:26 +0530

    Article component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./article.css"
 
-const Article = () => {
+// Parameter Destructuring
+const Article = ({ imgUrl, date, title }) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
